9d5ac50c106e421926aff067324974b10890978f
Prashitha <prashitha@zooserver.com>
2025-01-17 22:59:25 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,38 +1,56 @@
 $(document).ready(function() {
 
   $(".more-details-dot").click(function() {
-    $(".member-actions-dropdown").slideToggle();;
+    $(".member-actions-dropdown").slideToggle();
   })
 //  Open Popup cancel-member-ship
   $(".cancel-membership").on("click",function(){
-    $(".popup-overlay").show();
-    $(".popup").show();
+    $(".popup-overlay").fadeIn(400);
+    $(".popup-cancellation").fadeIn(400);
   });
 // close popup and Overlay
   $(".popup-close-icon").on("click",function(){
-    $(".popup-overlay").hide();
-    $(".popup").hide();
+    $(".popup-overlay").fadeOut(300);
+    $(".popup").fadeOut(300);
   });
 
   
+  $('.tab-button').click(function () {
+    var tabID = $(this).attr('data-tab');
+    $(this).addClass('active').siblings().removeClass('active');
+
+    $('#tab-' + tabID).addClass('active').siblings().removeClass('active');
+  });
+  
 });
 // to close dropdown Outer
-$(document).click(function() {
-  $(".member-actions-dropdown").hide();
-})
+$(document).click(function (event) {
+  if (!$(event.target).closest(".member-profile-deatils-box").length) {
+    $(".member-actions-dropdown").hide();
+  }
+});
 
-document.addEventListener("DOMContentLoaded", function () {
-  const accordionHeaders = document.querySelectorAll(".accordion-header-wrap");
+$(".member-profile-deatils-box").click(function (event) {
+  event.stopPropagation();
+});
 
-  accordionHeaders.forEach((header) => {
-    header.addEventListener("click", function () {
-      this.classList.toggle("active");
-      const content = this.nextElementSibling;
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      }
-    });
-  });
+$(".accordion-item").on("click", function () {
+  $(".accordion-content").slideUp();
+  $(this).toggleClass("active");
+  var content = $(this).find(".accordion-content");
+  if (content.is(":visible")) {
+    content.slideUp();
+  } else {
+    content.slideDown();
+  }
+});
+
+$(".edit-button-wrap").on("click", function(){
+  $(".popup-overlay").fadeIn(400);
+  $(".popup-edit-application").fadeIn(400);
+});
+
+$("#hamburger-icon").on("click", function(){
+  console.log($(this).closest(".outer-wrapper"))
+  $(this).closest(".outer-wrapper").find(".left-container").toggleClass("active");
 });
